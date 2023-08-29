7c7540051c2736d3b1158df87eb692c5dc82b756
akikuno <akihiro.kuno@gmail.com>
2023-08-29 20:42:35 +0900

    add `cli-test.yml`

---
+++ tests/append/tests.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+#######################################
+# From standart input
+#######################################
+
+# Short format
+cat tests/append/data/example.sam |
+    cstag append >/tmp/example_cs_short.sam
+
+# Long format
+cat tests/append/data/example.sam |
+    cstag append -l >tests/append/data/example_cs_long.sam
+
+if ! diff /tmp/example_cs_short.sam tests/append/data/example_cs_short.sam; then
+    exit 1
+fi
+
+if ! diff /tmp/example_cs_long.sam tests/append/data/example_cs_long.sam; then
+    exit 1
+fi
+
+#######################################
+# From file (SAM)
+#######################################
+
+# Short format
+cstag append -f tests/append/data/example.sam >/tmp/example_cs_short.sam
+
+# Long format
+cstag append -f tests/append/data/example.sam -l >tests/append/data/example_cs_long.sam
+
+if ! diff /tmp/example_cs_short.sam tests/append/data/example_cs_short.sam; then
+    exit 1
+fi
+
+if ! diff /tmp/example_cs_long.sam tests/append/data/example_cs_long.sam; then
+    exit 1
+fi
+
+#######################################
+# From file (BAM)
+#######################################
+
+# Short format
+cstag append -f tests/append/data/example.bam >/tmp/example_cs_short.sam
+
+# Long format
+cstag append -f tests/append/data/example.bam -l >tests/append/data/example_cs_long.sam
+
+if ! diff /tmp/example_cs_short.sam tests/append/data/example_cs_short.sam; then
+    exit 1
+fi
+
+if ! diff /tmp/example_cs_long.sam tests/append/data/example_cs_long.sam; then
+    exit 1
+fi
