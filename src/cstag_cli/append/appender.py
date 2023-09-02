cd9d0b3e22e70eff28564799942d26451a5ee29f
akikuno <akuno@md.tsukuba.ac.jp>
2023-09-02 12:00:09 +0900

    fix the args from is_long to long for cstag v0.5.1

--- src/cstag_cli/append/appender.py
+++ src/cstag_cli/append/appender.py
@@ -9,8 +9,8 @@ def append(sam, is_long: bool = False):
             print(read.to_string())
             continue
         if not read.has_tag("MD"):
-            raise ValueError(f"MD tag is not found in the input.")
+            raise ValueError("MD tag is not found in the input.")
         # append cstag and output alignments
-        cs = cstag.call(cigar=read.cigarstring, md=read.get_tag("MD"), seq=read.seq, is_long=is_long)
+        cs = cstag.call(cigar=read.cigarstring, md=read.get_tag("MD"), seq=read.seq, long=is_long)
         read.set_tag("cs", cs.replace("cs:Z:", ""), replace=True)
         print(read.to_string())
